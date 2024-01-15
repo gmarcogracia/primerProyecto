7f0788a46667d3b0bafd43cb02b56ac6fcb180fb
gmarcogracia <gmarcogracia2004@gmail.com>
2024-01-15 13:23:15 +0100

    Display Characters as list and added css

--- src/app/characters/characters.component.ts
+++ src/app/characters/characters.component.ts
@@ -1,12 +1,13 @@
 import { CHARACTERS } from './../mock-characters';
 import { Component } from '@angular/core';
 import { Character } from '../character';
-
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-characters',
   templateUrl: './characters.component.html',
-  styleUrl: './characters.component.css'
+  styleUrl: './characters.component.css',
+  
 })
 export class CharactersComponent {
   characters = CHARACTERS
